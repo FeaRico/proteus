226c689ce7ebae43cd350ac65341477f00a29753
Makhach Abdulazizov <abdulazizov2001@mail.ru>
2022-05-22 04:09:57 +0300

    Add controllers & swagger

---
+++ src/main/java/ru/makhach/vesselmanager/controller/CountryController.java
@@ -0,0 +1,48 @@
+package ru.makhach.vesselmanager.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.makhach.vesselmanager.model.dto.CountryDto;
+import ru.makhach.vesselmanager.service.facade.CountryServiceFacade;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/countries")
+public class CountryController {
+    private final CountryServiceFacade countryServiceFacade;
+
+    public CountryController(CountryServiceFacade countryServiceFacade) {
+        this.countryServiceFacade = countryServiceFacade;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CountryDto>> getAll() {
+        return ResponseEntity.ok(countryServiceFacade.getAllCountries());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CountryDto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(countryServiceFacade.getCountryById(id));
+    }
+
+    @GetMapping("/code/{code}")
+    public ResponseEntity<CountryDto> getByCode(@PathVariable String code) {
+        return ResponseEntity.ok(countryServiceFacade.getCountryByCode(code));
+    }
+
+    @PutMapping
+    public ResponseEntity<CountryDto> update(@RequestBody CountryDto country) {
+        return ResponseEntity.ok(countryServiceFacade.updateCountry(country));
+    }
+
+    @PostMapping
+    public ResponseEntity<CountryDto> save(@RequestBody CountryDto country) {
+        return ResponseEntity.ok(countryServiceFacade.saveCountry(country));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<CountryDto> delete(@PathVariable Long id) {
+        return ResponseEntity.ok(countryServiceFacade.deleteCountry(id));
+    }
+}
