b966664c49e7a33ae1ca4e94830311a377067d55
Makhach Abdulazizov <abdulazizov2001@mail.ru>
2022-08-14 00:38:36 +0300

    migrate to open api

---
+++ src/main/java/ru/makhach/proteus/config/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package ru.makhach.proteus.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .packagesToScan("ru.makhach")
+                .build();
+    }
+}
